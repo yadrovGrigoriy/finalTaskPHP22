860709702fff4d01a4784677c1fee3a56b0b3f6e
Grinch <anger@211.ru>
2018-05-31 01:11:41 +0700

    ref

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,33 @@
 */
 
 
-Route::get('/', 'QuestionsController@index');
 
-Auth::routes();
+
+
+  Auth::routes();
   Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', 'HomeController@index');
+    Route::resource('/admin/questions', 'AdminQuestionsController');
+    Route::resource('/admin/questions/store', 'AdminQuestionsController');
+    //Route::resource('/admin/questions/{question}/edit/', 'AdminQuestionsController');
+    Route::get('admin/questions/{question}/edit', 'AdminQuestionsController@edit');
+    Route::patch('admin/questions/{$question}', 'AdminQuestionsController@update');
+    // Route::resource('/admin/questions/{question}', 'AdminQuestionsController');
+    Route::delete('admin/questions/{question}', 'AdminQuestionsController@destroy');
+    route::resource('category', 'CategoryController');
+  });
+//    Route::get('/', 'QuestionsController@index');
+//    Route::get('/questions/create', 'QuestionsController@create');
+//    Route::post('/questions/store', 'QuestionsController@store');
+//    Route::get('/question/{question}/edit', 'QuestionsController@edit');
+//    Route::patch('/questions/{question}', 'QuestionsController@update');
+//    Route::delete('/questions/{question}', 'QuestionsController@destroy');
+//    Route::get('/{category}', 'CategoryController@index');
 
-      Route::get('/home', 'HomeController@index');
-
-      Route::get('/questions/create', 'QuestionsController@create');
-
-      Route::post('/questions/store', 'QuestionsController@store');
-
-      Route::get('/question/{question}/edit', 'QuestionsController@edit');
-
-      Route::patch('/questions/{question}', 'QuestionsController@update');
-
-      Route::delete('/questions/{question}', 'QuestionsController@destroy');
-
-      });
 
 
-
-
+  Route::get('/', 'QuestionsController@index');
+  Route::get('/questions/create', 'QuestionsController@create');
+  Route::post('/questions/store', 'QuestionsController@store');
 
 
