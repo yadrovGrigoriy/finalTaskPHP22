832460f2f30fab24f57c797b4627c95845d8a081
Grinch <anger@211.ru>
2018-06-09 00:46:40 +0700

    убрал сылки на гостевом

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
+	&nbsp;<div class="container">
+
+		<p><a class="btn btn-primary" href="{{ route(  'questions.index') }}">Список Вопросов</a>
+			<a class="btn btn-primary" href="{{ route( 'categories.index') }}">Список Категорий</a>
+			<a class="btn btn-primary" href="{{ route(      'users.index') }}">Список Пользователей</a></p>
+		<p><a class="btn btn-primary" href="{{ route(     'users.create') }}">Добавить Пользователя</a>
+			<a class="btn btn-primary" href="{{ route('categories.create') }}">Добавить Категорию</a>
+			<a class="btn btn-primary" href="{{ route( 'questions.create') }}">Добавить Вопрос</a></p>
+
+
+	</div>
 	<div class="container">
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
@@ -12,14 +23,17 @@
 								<tr>
 									<th>Категория</th>
 									<th>Колличество вопросов </th>
-									<th></th>
+									{{--<th>Обупликованные Вопросы</th>--}}
 									<th>Действия </th>
 								</tr>
 								@foreach($categories as $category)
 									<tr>
 										<td>{{ $category->category }}</td>
 										<td>{{ $category->questions->count() }}</td>
-										<td> {{$category->questions->count()}}</td>
+
+										@foreach($category->questions as $question)
+
+										@endforeach
 										<td>
 											<form action="{{ route('categories.destroy', $category->id) }}" method="POST">
 												<a type="button" class="btn btn-pri" href="{{ route('categories.edit', $category->id) }}">Редактировать</a>
@@ -28,7 +42,7 @@
 												<button type="submit" class="btn btn-danger">Удалить</button>
 											</form>
 										</td>
-									</tr>
+										</tr>
 								@endforeach
 							</table>
 						@else
