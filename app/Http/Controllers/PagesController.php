d5ef0eae07ef9e2522d12128d40d343602b23994
Grinch <anger@211.ru>
2018-06-09 00:44:12 +0700

    гостевой вход

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Category;
+use App\Question;
 
 class PagesController extends Controller
 {
-    public function  index()
-    {
-      return view('welcome');
-    }
+//    public function  index()
+//    {
+//      return redirect()->route('index');
+//    }
+
+  public function index()
+  {
+    $categories = Category::all();
+    $questions = Question::all();
+    return view('index', compact( 'categories', 'questions'));
+  }
 }
